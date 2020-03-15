1c6f837a18087796f79e79698e78582fa4de71d8
pakam2 <pkami.87@gmail.com>
2020-03-15 20:29:12 +0100

    JS task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0;
+  let flag = false;
+  let configLen = bracketsConfig.length;
+  while (i < configLen) {
+    flag = false;
+    let bracketsJoin = bracketsConfig[i].join('');
+    if (str.includes(bracketsJoin)) {
+      str = str.replace(bracketsJoin, '')
+      flag = true;
+    }
+    if (flag) {
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+  if (str.length) {
+    return false;
+  }
+
+  return true;
+
 }
